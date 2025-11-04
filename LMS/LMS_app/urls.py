bbda058560db4748dd601ab8cc8807eed5508711
kanyakunjumon <150785632+kanyakunjumon@users.noreply.github.com>
2025-11-04 11:39:38 +0530

    server runnng

--- LMS/LMS_app/urls.py
+++ LMS/LMS_app/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 from . import views
+from .views import server_running
 
 urlpatterns = [
 
+    path('', server_running, name='server-running'),
+
     #enquiry API endpoints
+    path('api/enquiries/', views.EnquiryListCreateView.as_view(), name='enquiry-create-list'),
     path('api/enquiries/', views.EnquiryListCreateView.as_view(), name='enquiry-create-list'),
     path('api/enquiries/<int:pk>/', views.EnquiryDetailView.as_view(), name='enquiry-retrieve-update-destroy'),
 
--- LMS/LMS_app/views.py
+++ LMS/LMS_app/views.py
@@ -4,6 +4,13 @@ from .models import *
 from .serializers import *
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.decorators import api_view
+
+@api_view(['GET'])
+def server_running(request):
+    return Response({"message": "Server running"})
+
+
 
 #certfication
 class CertficationListCreateView(generics.ListCreateAPIView):
